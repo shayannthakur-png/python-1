ec3cded700a51444bf48ea1144e726a6cf0fb452
shayannthakur-png <shayannthakur@gmail.com>
2026-01-23 18:06:46 +1100

    Add files via upload

---
+++ second.py
@@ -0,0 +1,6 @@
+print("hi how are you look at this space \n look" )
+
+print(4)
+
+print("please write your name \n here")
+print("bye bye ",end="*")
---
+++ variables.py
@@ -0,0 +1,8 @@
+num="hello "
+print(num)
+hum="hi"
+print(num ,hum )
+shay="im in school"
+print(num ,shay)
+name=input("enter name here:")
+print("\nhello",name,"\n welcome to this area")
