d3e691786490cc8fdc5185b6e7a9eb9e69876e74
shayannthakur-png <shayannthakur@gmail.com>
2026-01-28 18:04:47 +1100

    Add files via upload

---
+++ data.py
@@ -0,0 +1,6 @@
+a=123
+b=1.1
+c=True
+print(type(a))
+print(type(b))
+print(type(c))
---
+++ data2.py
@@ -0,0 +1,26 @@
+name= "jeff"
+age= 13
+is_student= True
+weight= 47.1
+
+print("name:", name)
+print("data type of name is", type(name))
+
+print("age:", age)
+print("data type of age is", type(age))
+
+print("is_student", is_student)
+print("data type of if he is a stufen is", type(is_student))
+
+print("weight", weight)
+print("data type of the weight is", type(weight))
+print("\n after type casting. . . .")
+
+input("hi ")
+text = str(input("enter a string: " ))
+
+revText = text[::-1]
+text = revText
+
+print("revers of given string is:")
+print(text)
