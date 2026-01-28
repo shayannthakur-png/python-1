008b8be844bcdd22fe89501290c6170623f60817
shayannthakur-png <shayannthakur@gmail.com>
2026-01-28 18:10:47 +1100

    Add files via upload

--- first.py
+++ first.py
@@ -1,5 +1,2 @@
-print("hi")
-print("my name is shayann")
-print("i love playing soccer")
-#add two numbers
-print(134250+123451)
+rajbirthday= ("28th june", "25th june","1st june "," 2nd march","3rd march")
+print(rajbirthday)
