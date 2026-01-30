22460114c194cca279e4b0c2fd7cdb6ac8b43c27
shayannthakur-png <shayannthakur@gmail.com>
2026-01-30 15:22:36 +1100

    Add files via upload

--- second.py
+++ second.py
@@ -1,6 +1,4 @@
-print("hi how are you look at this space \n look" )
-
-print(4)
-
-print("please write your name \n here")
-print("bye bye ",end="*")
+num="congrats"
+num2="CONGRATS"
+print(num )
+print(num2)
