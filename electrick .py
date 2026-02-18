ec9d50e94cb3b65eb7e67194ebefb9be32eb583f
shayannthakur-png <shayannthakur@gmail.com>
2026-02-18 17:57:31 +1100

    electrick .py

--- electrick .py
+++ electrick .py
@@ -7,3 +7,16 @@ if uni < 50:
 elif uni <= 100:
     amount = 130 + ((uni - 50) * 3.25)
     surcharge = 35
+elif uni <= 100:
+    amount = 130 + ((uni - 50) * 3.25)
+    surcharge = 35
+
+elif uni <= 200:
+    amount = 130 + 162.50 + ((uni - 100) * 5.26)
+    surcharge = 45
+
+else:
+    amount = 130 + 162.50 + 526 + ((uni - 200) * 8.45)
+    surcharge = 75
+
+total = amount + surcharge
