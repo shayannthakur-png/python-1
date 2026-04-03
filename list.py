69d78eff19c499e5ccc5c4dad9e957859dde12fc
shayannthakur-png <shayannthakur@gmail.com>
2026-04-03 18:14:06 +1100

    Add files via upload

---
+++ list.py
@@ -0,0 +1,16 @@
+# Create an empty list
+empty_list = []
+print(empty_list)
+
+# Create a list of numbers
+numbers = [1, 2, 3, 4, 5]
+print(numbers)
+
+# Use * operator to repeat list elements
+triples = [1, 2, 3] * 3
+print(triples)
+
+# Reverse the given list
+alist = [100, 200, 300, 400, 500]
+alist = alist[::-1]
+print(alist, "\n")
